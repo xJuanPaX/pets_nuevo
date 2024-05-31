8ba9083886fa6553f93deef48b64e6c958992a39
Juan <jperazo.1236@unicesmag.edu.co>
2024-05-31 17:05:08 -0500

    actualizacion repositorio

---
+++ src/list_users.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        <h1>
+           LIST USERS 
+        </h1>
+    </center>
+    <table class="table table-striped">
+        <tr>
+            <th>Fullname</th>
+            <th>Email</th>
+            <th>Status</th>
+            <th>Foto</th>
+            <th>...</th>
+        </tr>
+        <tr>
+            <td>Samir Alih</td>
+            <td>example@example.com</td>
+            <td>Active</td>
+            <td>
+                <img src="Icons/usuario.png" width="32">
+            </td>
+            <td>
+                <a href="#"><img src="Icons/edit.png" width="32"></a>
+                <a href="#"><img src="Icons/delete.png" width="32" height="32"></a>
+            </td>
+        </tr>
+    </table>
+</body>
